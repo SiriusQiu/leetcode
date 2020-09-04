2de0d9dc76da1f77f561b9c4735eb392a1b8ad13
sirius <sirius@cqupt>
2020-09-04 17:05:40 +0800

    kmp

---
+++ src/main/java/com/cqupt/sirius/leetcode/day0904/Main.java
@@ -0,0 +1,7 @@
+package com.cqupt.sirius.leetcode.day0904;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/cqupt/sirius/leetcode/day0904/kmp/Main.java
@@ -0,0 +1,32 @@
+package com.cqupt.sirius.leetcode.day0904.kmp;
+
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(new BufferedInputStream(System.in));
+        int n = scanner.nextInt();
+        String p = scanner.next();
+        int m = scanner.nextInt();
+        String s = scanner.next();
+        //next指的是，到当前序号中，最长前缀的序号
+        int[] next = new int[n];
+        next[0] = -1;
+        for (int i = 1, j = -1; i < n; i++){
+            //两种中止情况：1. j为0，2, i=j+1，如果i=j+1，那么就是next[i]=j+1,如果
+            while (j!=-1&&p.charAt(i)!=p.charAt(j+1)) j = next[j];
+            if (p.charAt(i)==p.charAt(j+1)) j++;
+            next[i] = j;
+        }
+        for (int i = 0, j = -1; i < m; i++){
+            while (j!=-1&&s.charAt(i)!=p.charAt(j+1)) j = next[j];
+            if (s.charAt(i)==p.charAt(j+1)) j++;
+            if (j==n-1) {
+                System.out.print((i - n + 1) + " ");
+                j = next[j];
+            }
+        }
+    }
+}
